070063b939822573d90789f04bcad68de1edb0ee
Ahmad ATWI <aatwi@murex.com>
2023-01-16 17:16:06 +0100

    Provide an example of a working feature

--- src/test/java/com/gildedrose/StepDefinitions.java
+++ src/test/java/com/gildedrose/StepDefinitions.java
@@ -3,12 +3,28 @@ package com.gildedrose;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Assertions;
 
-import static org.junit.Assert.assertEquals;
 
 public class StepDefinitions {
     private Item[] items = new Item[1];
     private GildedRose app;
 
+    private String emailAddress;
+    private boolean emailAddressIsValid = false;
+    @Given("A user enters the email address {string}")
+    public void aUserEntersTheEmailAddress(String email) {
+        this.emailAddress = email;
+    }
+
+    @When("The system verifies the email address")
+    public void theSystemVerifiesTheEmailAddress() {
+        this.emailAddressIsValid = emailAddress.contains("@");
+    }
+
+    @Then("Email address is accepted")
+    public void assertValidityOfEmailAddress() {
+        Assertions.assertTrue(emailAddressIsValid);
+    }
 }
 
--- src/test/resources/com.gildedrose/GildedRose.feature
+++ src/test/resources/com.gildedrose/GildedRose.feature
@@ -6,3 +6,10 @@ Feature: Gilded Rose Inn
       Given # Write the initial context of the scenario/example
       When # Write the event or action to be applied
       Then # Write the expected outcome or result
+
+  # Use the below example to ensure the wiring between the feature file and the definitions code is working as expected
+  Rule: This is an example an email address verification system
+    Scenario: Email address must include the '@' sign
+      Given A user enters the email address "John.Doe@example.com"
+      When The system verifies the email address
+      Then Email address is accepted
